fcd95777be38be68ce4136411e36759a29fc9a28
Lucila Alochis <lucila.alochis1999@gmail.com>
2023-03-16 18:27:23 -0300

    tabla y details pasados a vue, eventos past, opcoming e index también

---
+++ main.js
@@ -0,0 +1,49 @@
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+            valorBusqueda: '',
+            categorias: [],
+            eventos: [],
+            checked: [],
+            eventosFiltrados: [],
+            cDate: [],
+            idEvento: [],
+            details: []
+    
+        };
+    },
+    created() {
+        fetch('https://mindhub-xj03.onrender.com/api/amazing')
+            .then(response => response.json())
+            .then(data => {
+
+                this.cDate = data.currentDate
+                this.eventos = data.events.filter(evento => evento.category)
+                this.eventosFiltrados = data.events.filter(evento => evento.category)
+                this.categorias = [... new Set(data.events.filter(evento => evento.category).map(evento => evento.category))]
+                const params = new URLSearchParams(location.search)
+                const id = params.get("id")
+                this.details = data.events.find(evento => evento._id == id);
+
+
+            })
+            .catch(err => {
+                console.log('entro en el catch')
+            })
+    },
+
+    methods: {
+     
+        filtro() {
+            let filtradoBusqueda = this.eventos.filter (evento => evento.name.toLowerCase().includes(this.valorBusqueda.toLowerCase()))
+            let filtradoCheck = filtradoBusqueda.filter (evento => this.checked.includes(evento.category) || this.checked.length == 0)
+           this.eventosFiltrados = filtradoCheck
+        },
+   
+       
+    },
+})
+
+app.mount('#app')
---
+++ stats.js
@@ -0,0 +1,63 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            events: [],
+            largerCapacity: {},
+            lowestPercentage: {},
+            higherPercentage: {},
+            upcommingStats: [],
+            pastStats: [],
+            loadData: false,
+            failPromise:null
+        }
+    },
+    created() {
+        fetch('https://mindhub-xj03.onrender.com/api/amazing')
+            .then(response => response.json())
+            .then(data => {
+             
+                this.events = data.events
+                this.createGeneralStats()
+                this.createCategoryStats(this.events.filter(e => e.estimate), this.upcommingStats)
+                this.createCategoryStats(this.events.filter(e => e.assistance), this.pastStats)
+                this.loadData = true
+            }
+            )
+            .catch(err=>this.failPromise=true)
+    },
+    methods: {
+       accumulator(eventList) {
+            let accum = { "revenues": 0, "percentArray": [], "prom": 0 };
+            let totalPercent = 0;
+            let count = 0;
+            
+            eventList.forEach(e => {
+              accum.revenues += (e.estimate ? e.estimate : e.assistance) * e.price;
+              accum.percentArray.push(((e.estimate ? e.estimate : e.assistance) * 100 / e.capacity));
+              
+              totalPercent += accum.percentArray[count];
+              count++;
+            });
+            
+            accum.prom = (totalPercent / accum.percentArray.length).toFixed(2);
+            return accum;
+          },
+        createCategoryStats: function (events, list) {
+            let setCategory = Array.from(new Set(events.map(e => e.category).sort()))
+            setCategory.forEach(category => {
+                let categoryStats = this.accumulator(events.filter(e => e.category === category))
+                categoryStats.name = category
+                list.push(categoryStats)
+            })
+        },
+        createGeneralStats: function(){
+                this.largerCapacity = this.events.sort((e1, e2) => e2.capacity - e1.capacity)[0]
+                this.events.forEach(e => e.percent = (e.assistance * 100 / e.capacity).toFixed(2))
+                let evsByAttPcent = this.events.filter(e => e.assistance).sort((e1, e2) => e1.percent - e2.percent)
+                this.lowestPercentage = evsByAttPcent[0]
+                this.higherPercentage = evsByAttPcent[evsByAttPcent.length - 1]
+        }
+    }
+}).mount('#app')
